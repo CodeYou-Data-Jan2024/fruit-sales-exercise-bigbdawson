55299f47a5929823e124cec59808e53011982f20
bigbdawson <bigbdawson@gmail.com>
2024-05-13 01:47:29 -0400

    Fruit Fix

--- fruitsales.py
+++ fruitsales.py
@@ -1,6 +1,7 @@
 import pandas as pd
-fruit_sales = pd.DataFrame({'Apples' : [35,41], 'Bananas' : [21,34]}, index=['2017 Sales', '2018 Sales'])
 
-print(fruit_sales)
+df = pd.DataFrame({'Apples' : [35,41], 'Bananas' : [21,34]}, index=['2017 Sales', '2018 Sales'])
 
-fruit_sales.to_csv('fruit.csv')
+print(df)
+
+df.to_csv('fruit.csv')
